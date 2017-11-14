9e388c4a1b982f31871171eef24c4a021b334a2b
Quentin Chevrin <quentin.chevrin@gmail.com>
2017-11-14 15:42:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -115,9 +115,15 @@ class SignaturePad extends Component {
 
   };
 
+  onMessage = (event) => {
+    var base64DataUrl = JSON.parse(event.nativeEvent.data);
+    this._bridged_finishedStroke(base64DataUrl);
+  }
+
   render = () => {
     return (
         <WebView automaticallyAdjustContentInsets={false}
+                 onMessage={this.onMessage}
                  onNavigationStateChange={this._onNavigationChange}
                  renderError={this._renderError}
                  renderLoading={this._renderLoading}
